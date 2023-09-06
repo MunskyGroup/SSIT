c364cf8d2fc921cd88ace04d6766605f1485475f
Munsky <munsky@engr.colostate.edu>
2023-09-06 15:19:05 -0600

    Add more tests for SSIT.

--- tests/ssitTests.m
+++ tests/ssitTests.m
@@ -63,12 +63,16 @@ classdef ssitTests < matlab.unittest.TestCase
 
     methods (Test)
         function ModelCreation(testCase)
+            % In this trivial test, we check that the SSIT is set up with
+            % the right names for the 'rna' species.
             nm = testCase.Poiss.species;
             testCase.verifyEqual(nm{1}, 'rna', ...
                 'Species name is incorrect');
         end
 
         function FspConverged(testCase)
+            % In this test, we check tha tthe FSP solution exits with an
+            % appropriate FSP tolerance value.
             final = testCase.PoissSolution.fsp{end}.p.sum;
             tst = (1-final)<=testCase.Poiss.fspOptions.fspTol;
             testCase.verifyEqual(tst, true, ...
@@ -76,6 +80,9 @@ classdef ssitTests < matlab.unittest.TestCase
         end
 
         function PoissonMeans(testCase)
+            % In this test, we check that the Time invariant 1D Poisson
+            % model generates a solution with the correct means versus
+            % time. 
             t = testCase.Poiss.tSpan;
             mn = testCase.Poiss.parameters{1,2}/testCase.Poiss.parameters{2,2}*...
                 (1-exp(-testCase.Poiss.parameters{2,2}*t));
@@ -100,6 +107,8 @@ classdef ssitTests < matlab.unittest.TestCase
         end
 
         function TimeVaryingPoissonMeansSuccess(testCase)
+            % In this test, we check that the 1D Time Varying Poisson model
+            % generates a solution with the correct mean versus time.
             t = testCase.TvPoiss.tSpan;
             tp = max(0,t-1);
             
@@ -121,11 +130,15 @@ classdef ssitTests < matlab.unittest.TestCase
         end
 
         function TimeVaryingPoissonSpeedSuccess(testCase)
+            % In this test, we check that the Time Varying 1D Poisson model
+            % is solved in a reasonable amount of time.
             testCase.verifyEqual(testCase.TvPoissSolution.time<0.4, true, ...
                 'Time Varying Possion Solution Time is Slow');
         end
         
         function TwoDPoissMeansSuccess(testCase)
+            % In this test, we check that the 2D Poisson model generates a
+            % solution with the correct means versus time.
             t = testCase.TwoDPoiss.tSpan;
             mn1 = testCase.TwoDPoiss.parameters{1,2}/testCase.TwoDPoiss.parameters{2,2}*...
                 (1-exp(-testCase.TwoDPoiss.parameters{2,2}*t));
@@ -152,11 +165,17 @@ classdef ssitTests < matlab.unittest.TestCase
         end
 
         function TwoDPoissonSpeedSuccess(testCase)
+            % In this test, we check that the 2D Poisson model is solved in
+            % a reasonable amount of time.
             testCase.verifyEqual(testCase.TwoDPoissSolution.time<0.3, true, ...
                 'TwoD Possion Solution Time is Slow');
         end
 
         function HybridModel(testCase)
+            % In this test, we check that the code correctly solves the
+            % hybrid 2D Poisson Model, where the first species is solved
+            % using ODE model and the second species is solved using the
+            % discrete stochastic analysis.
 
             HybridModel = testCase.TwoDPoiss;
             HybridModel.useHybrid = true;
@@ -189,18 +208,23 @@ classdef ssitTests < matlab.unittest.TestCase
         end
 
         function EscapeTimeCalculation(testCase)
+            % In this test, we check that the code correctly calcuates the
+            % escape times until N births occur in the pure birth model.
+            % Here, we compare to the exact solution:
+            % P(t) = gamma(t|N,1/k)
 
             escapeModel = testCase.Poiss;
             escapeModel.parameters{2,2} = 0;
+            t = escapeModel.tSpan;
+            k = escapeModel.parameters{1,2};
+            n = 10;
             escapeModel.fspOptions.escapeSinks.f = {'x1'};
-            escapeModel.fspOptions.escapeSinks.b = 10;
+
+            escapeModel.fspOptions.escapeSinks.b = n-0.1;
             [escapeSoln,escapeModel.fspOptions.bounds] = escapeModel.solve;
             
-            t = escapeModel.tSpan;
-            a = escapeModel.parameters{1,2};
-            n = 10;
 
-            exact = cdf('gamma',t,n+1,1/a);
+            exact = cdf('gamma',t,n,1/k);
 
             fspSoln = escapeSoln.fsp;
             escapeProbFSP = NaN*exact;
@@ -218,12 +242,17 @@ classdef ssitTests < matlab.unittest.TestCase
         end
 
         function Plotting(testCase)
+            % In this test, we check that the code successfully generates
+            % fogures for the solutions of the 2D Poisson Model. If
+            % successful, four figures should have been generated.
             testCase.TwoDPoiss.makePlot(testCase.TwoDPoissSolution,'meansAndDevs',[],[],1);
             testCase.TwoDPoiss.makePlot(testCase.TwoDPoissSolution,'marginals',[3:3:21],[],[2,3]);
-            testCase.TwoDPoiss.makePlot(testCase.TwoDPoissSolution,'joints',[3:3:21],[],[4]);
+            testCase.TwoDPoiss.makePlot(testCase.TwoDPoissSolution,'joints',[3:3:21],[],4);
         end
 
         function FspDataGeneration(testCase)
+            % In this test, we check that the code generates and saves data
+            % generated using the Poisson model.
             delete 'testData.csv'
             testCase.Poiss.ssaOptions.nSimsPerExpt = 1000;
             testCase.Poiss.ssaOptions.Nexp = 1;
@@ -234,6 +263,10 @@ classdef ssitTests < matlab.unittest.TestCase
         end
 
         function DataLoading(testCase)
+            % In this test, we check that the code correctly loads the data
+            % geerated using the Poisson model. 
+            % Note - this will only work if the code successfully generated
+            % the data in the first place.
             Z = double(testCase.Poiss.dataSet.app.DataLoadingAndFittingTabOutputs.dataTensor);
             n = sum(Z(1,:));
             ssaMn = Z*[0:size(Z,2)-1]'/n;
@@ -252,6 +285,10 @@ classdef ssitTests < matlab.unittest.TestCase
         end
 
         function ComputingLikelihood(testCase)
+            % In this test we compare the computed log-likelihood to the
+            % exact solution for the Poisson Model:
+            % lam(t) = k/g*(1-exp(-g*t));
+            % logL = prod_n [Poisson(n|lam(t))]
             fspLogL = testCase.Poiss.computeLikelihood;
             
             t = [testCase.Poiss.dataSet.DATA{:,1}];
@@ -265,6 +302,78 @@ classdef ssitTests < matlab.unittest.TestCase
 
             testCase.verifyEqual(relDiff<0.0001, true, ...
                 'Likelihood Calculation is not within 0.01% Tolerance');            
+        end
+
+        function ComputingSensitivities(testCase)
+            % In this test, we compare the poisson model sensitivites to
+            % the parameters 'k' and 'g' using the exact solution:
+            % lam(t) = k/g*(1-exp(-g*t));
+            % Sk(t) = dP(n|t)/dk = 1/g*(1-exp(-g*t)) * Poiss(n|lam(t)) * (n/lam - 1)
+            % Sg(t) = dP(n|t)/dg = (-k/g^2*(1-exp(-g*t))+k*t/g*exp(-g*t)) * Poiss(n|lam(t)) * (n/lam - 1)
+
+            Model = testCase.Poiss;
+            Model.solutionScheme = 'fspSens';
+            Model.fspOptions.fspTol = 1e-6;
+            SensSoln = Model.solve;
+            t = testCase.Poiss.tSpan;
+            k = Model.parameters{1,2};
+            g = Model.parameters{2,2};
+            lam = k/g*(1-exp(-g*t))';
+
+            fspSensSoln = SensSoln.sens.data;
+            for i = 2:length(fspSensSoln)
+                n = size(SensSoln.sens.data{i}.S(1).data);
+                analytical1 = pdf('poiss',(0:n-1),lam(i))'.*([0:n-1]'/lam(i) - 1).*...
+                    (1/g)*(1-exp(-g*t(i)));
+                fspSens1 = double(SensSoln.sens.data{i}.S(1).data);
+                diff1(i) = sum(abs(analytical1-fspSens1));
+                
+                analytical2 = pdf('poiss',(0:n-1),lam(i))'.*([0:n-1]'/lam(i) - 1).*...
+                    ((-k/g^2)*(1-exp(-g*t(i)))+k/g*t(i)*exp(-g*t(i)));
+                fspSens2 = double(SensSoln.sens.data{i}.S(2).data);
+                diff2(i) = sum(abs(analytical2-fspSens2));
+                
+            end
+
+            Model.makePlot(SensSoln,'marginals',21,[],[5]);
+
+            testCase.verifyEqual(max(diff1+diff2)<0.001, true, ...
+                'Sensitivity Calculation is not within 0.1% Tolerance');            
+
+        end
+
+        function TestFIM(testCase)
+            % In this test case, we check that the FIM calculation using
+            % the FSP matches to the analytical expresion for the Poisson
+            % model:
+            % lam(t) = k/g*(1-exp(-g*t));
+            % FIM_kk = (dlam/dk)^2 * FIM_lamlam = lam/k^2
+            % FIM_gg = (dlam/dg)^2 * FIM_lamlam = (-lam/g + k*t/g*exp(-g*t))
+            % FIM_kg = (dlam/dg)(dlam/dk) * FIM_lamlam = 
+            %                                 (-lam/g + k*t/g*exp(-g*t))/k
+            Model = testCase.Poiss;
+            Model.solutionScheme = 'fspSens';
+            Model.fspOptions.fspTol = 1e-6;
+            SensSoln = Model.solve;
+            fspFIM = Model.computeFIM(SensSoln.sens);
+            t = Model.tSpan;
+            k = Model.parameters{1,2};
+            g = Model.parameters{2,2};
+            lam = k/g*(1-exp(-g*t))';
+            for i = 2:length(t)
+                exactIk(i) = lam(i)/k^2;
+                fspIk(i) = fspFIM{i}(1,1);
+                exactIg(i) = (-lam(i)/g + k*t(i)/g*exp(-g*t(i)))^2/lam(i);
+                fspIg(i) = fspFIM{i}(2,2);
+                exactIkg(i) = (-lam(i)/g + k*t(i)/g*exp(-g*t(i)))/k;
+                fspIkg(i) = fspFIM{i}(1,2);
+            end
+                
+            diff = max(abs(exactIk-fspIk)+abs(exactIg-fspIg)+abs(exactIkg-fspIkg));
+            testCase.verifyEqual(diff<0.001, true, ...
+                'FIM Calculation is not within 1e-4% Tolerance');            
+
+
         end
 
     end
