871b4014a66d56673f10f2ed332034062f3deb45
Munsky <munsky@engr.colostate.edu>
2024-03-13 17:49:44 -0600

    Update batch_run_sequential_experiment_analyses.sh

--- WorkSpace/sequentialEDcdc/batch_run_sequential_experiment_analyses.sh
+++ WorkSpace/sequentialEDcdc/batch_run_sequential_experiment_analyses.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-#!/bin/bash
 #SBATCH --job-name=my_matlab_job
 #SBATCH --output=my_matlab_job.out
 #SBATCH --error=my_matlab_job.err
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
+#SBATCH --partition=all
 
 module load matlab-2022b  # Load MATLAB module if necessary
-
-# Write MATLAB script to a temporary file
-echo "$MATLAB_SCRIPT" > func.m
 
 # Loop over combinations of i and j
 for i in {1..3}; do
@@ -17,7 +14,6 @@ for i in {1..3}; do
     	for k in {1..3}; do
     	    LOG_FILE="log_${i}_${j}_${k}.txt"
         	# Submit MATLAB job to Slurm for each combination
-        	srun --exclusive matlab -nodisplay -r "sequentialExptDesignBatchRunner($i, $j, $k); exit;" > "$LOG_FILE" &
-        	echo "Submitted MATLAB job with ID: $JOB_ID for i=$i and j=$j and k=$k"
+        	matlab -nojvm -nodisplay -r "sequentialExptDesignBatchRunner($i, $j, $k); exit;" > "$LOG_FILE" &
      done
 done
