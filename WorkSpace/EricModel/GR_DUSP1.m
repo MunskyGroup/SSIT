ca6ea0c61fb234d064659781f9ffe262e627b85c
Alex Popinga <alexpopinga@gmail.com>
2024-07-26 20:01:38 -0600

    Complete separation of plot functions makeGRPlots and plotODEresults
    Moved the functions makeGRPlots and plotODEresults into separate files to be called by GR_DUSP1 and TS_Triptolide_dynamics as necessary

--- WorkSpace/EricModel/GR_DUSP1.m
+++ WorkSpace/EricModel/GR_DUSP1.m
@@ -7,7 +7,6 @@
 %   2) replica to replica variations are expected that would result in
 %   slightly different parameter combinations
 close all 
-clear all
 addpath(genpath('../../src'));
 
 loadPrevious = true;
@@ -141,7 +140,7 @@ fimGR_withPrior = combinedGRModel.FIM.totalFIM+... % the FIM in log space.
 % delete(MHFitOptions.saveFile)
  
 %%     STEP 1.D. -- Make Plots of GR Fit Results
-extraGRFunctions.makeGRPlots(combinedGRModel,GRpars)
+makeGRPlots(combinedGRModel,GRpars)
 
 %%
 %%  STEP 2 -- Extend Model to Include DUSP1 Activation, Production, and Degradation
@@ -441,7 +440,7 @@ end
 
 %%    STEP 3.F.3. -- Plot all results
 % Plot GR Distribution
-GR_plot_functions.makeGRPlots(combinedGRModel,fullPars(5:12))
+makeGRPlots(combinedGRModel,fullPars(5:12))
 
 % Plot DUSP1 100nm FIT and other PREDICTED distributions
 showCases = [1,1,1,1];
@@ -560,35 +559,6 @@ save('workspaceJuly24',varNames{:})
 %    ModelGroup{i}.makeFitPlot([],1,[],true,'STD')
 %end
 %end
-
-function plotODEresults(extendedMod,soln,modeWithGRData,fignum)
-arguments
-    extendedMod
-    soln
-    modeWithGRData
-    fignum = 1;
-end
-figure(fignum); clf;
-% Plot GR levels vs. Time
-subplot(2,1,1)
-plot(extendedMod.tSpan,soln.ode(:,3:4),'--','LineWidth',2);hold on
-plot(modeWithGRData.dataSet.times,modeWithGRData.dataSet.mean,'s','MarkerSize',16,'MarkerFaceColor','k','LineWidth',3)
-legend(extendedMod.species(3:4))
-set(gca,'xlim',[-10,200],'ylim',[0,12],'fontsize',16)
-ylabel('GR Concentrations (UA)')
-legend({'Cyt-Model','Nuc-Model','Cyt-Data','Nuc-Data'})
-title('GR')
-
-% Plot DUSP1 levels vs. Time
-subplot(2,1,2)
-plot(extendedMod.tSpan,soln.ode(:,5:6),'--','LineWidth',2);hold on
-plot(extendedMod.dataSet.times,extendedMod.dataSet.mean,'s','MarkerSize',16,'MarkerFaceColor','k','LineWidth',3)
-legend(extendedMod.species(3:4))
-set(gca,'xlim',[-10,200],'ylim',[0,160],'fontsize',16)
-ylabel('GR Concentrations (UA)')
-legend({'Nuc-Model','Cyt-Model','Nuc-Data','Cyt-Data'})
-title('DUSP1')
-end
 
 function makeCytDistPlots(ssaSoln_100,extendedMod,fignum,timeIndsMod,timeIndsDat,speciesIndMod,speciesIndDat)
 arguments
