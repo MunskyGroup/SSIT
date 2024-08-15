8e881f864b8b2632c7d39fd3c55c54c27760e195
Munsky <munsky@engr.colostate.edu>
2024-08-15 11:59:48 -0600

    Update example_ToggleSwitch.m

--- Examples/example_ToggleSwitch.m
+++ Examples/example_ToggleSwitch.m
@@ -8,21 +8,22 @@ addpath(genpath('../src'));
 %% Create SSIT Model
 A = SSIT();    % Create SSIT instance using pre-selected model
 A.species = {'lacI';
-             'lambdaCI'};   % Set species names.
+             'lambdaCI'};   % Set species names
+A.initialCondition = [4;0]; % Set initial condition
 A.parameters = {'kb',10;
                 'ka',80;
                 'M',20;
-                'g',1}; % Set parameter names and values
+                'g',1};   % Set parameter names and values
 A.stoichiometry = [1,-1,0, 0;...
                    0, 0,1,-1]; % Set Stoichiometry matrix
 A.propensityFunctions = {'kb+ka*M^3/(M^3+lambdaCI^3)';
                          'g*lacI';...
                          'kb+ka*M^3/(M^3+lacI^3)';
                          'g*lambdaCI'}; % Set propensity functions
-A.initialCondition = [4;0]; % Set initial condition
-A.customConstraintFuns = {'(lacI-3).^2.*(lambdaCI-3).^2'}; % Set FSP constraint.
-A.tSpan = [0:3:12];          % Set times at which to compute distributions
-A = A.formPropensitiesGeneral('ToggleModel');
+A.customConstraintFuns = {'(lacI-3).^2.*(lambdaCI-3).^2'}; % Add FSP constraint
+A.tSpan = [0:3:12];    % Set times at which to compute distributions
+A.summarizeModel       % Print model summary to screen
+A = A.formPropensitiesGeneral('ToggleModel'); % Generate codes model
 
 %% Solve using the FSP approach
 A.solutionScheme = 'FSP';    % Set solutions scheme to FSP.
@@ -31,12 +32,12 @@ A.fspOptions.fspTol = 1e-4;  % Set FSP error tolerance.
 A.makePlot(FSPsoln,'marginals',[2:5],false,[1,2])  % Plot marginal distributions
 A.makePlot(FSPsoln,'joints',[2:5],false,[5])       % Plot joint distributions
 
-%% Solve Sensitivity using FSP
+%% Solve for the CME Sensitivity using FSP
 A.solutionScheme = 'fspSens'; % Set solutions scheme to FSP Sensitivity
 [sensSoln] = A.solve(FSPsoln.stateSpace);  % Solve the sensitivity problem
 A.makePlot(sensSoln,'marginals',[],false,[3,4]) % Plot marginal sensitivities
 
-%% Define a Binomial PDO
+%% Define a Binomial Probabilistic Distortion Operator
 A.pdoOptions.type = 'Binomial';
 A.pdoOptions.props.CaptureProbabilityS1 = 0.7;  % Distortion for S1
 A.pdoOptions.props.CaptureProbabilityS2 = 0.7; % Distortion for S2
@@ -50,14 +51,14 @@ A.makePlot(FSPsoln,'marginals',[2:5],true,[1,2])  % Plot Distorted Marginals
 A.solutionScheme = 'fspSens'; % Set solution scheme to Sensitivity
 A.makePlot(sensSoln,'marginals',[],true,[3,4])    % Plot Distorted Sensitivities
 
-%% Simulate Data for Subsequent Fitting
+%% Generate Simulated Data for Subsequent Fitting
 A.solutionScheme = 'FSP';  % Set solution scheme to SSA.
 A.ssaOptions.Nexp = 150;   % Number of independent data sets to generate.
 A.ssaOptions.nSimsPerExpt = 200; % Number of cells to include at each time point for each data set.
 A.ssaOptions.applyPDO = true; % Include the distortion in the SSA data.
 A.sampleDataFromFSP(FSPsoln,'ToggleSSAData50Expts.csv')  % Generate and save all data.
 
-%% Associate Datasets with FSP Models
+%% Associate Each Dataset with an FSP Model 
 A.solutionScheme = 'FSP';  % Set solution scheme back to FSP.
 B{1} = A.loadData('ToggleSSAData50Expts.csv',{'lacI','exp1_s1';'lambdaCI','exp1_s2'});
 B{1}.pdoOptions.PDO = [];  % Do not use PDO.
@@ -66,7 +67,10 @@ B{2}.pdoOptions.PDO = [];  % Do not use PDO.
 B{3} = A.loadData('ToggleSSAData50Expts.csv',{'lacI','exp1_s1_Distorted';'lambdaCI','exp1_s2_Distorted'});
 
 %% Plot Results
-for m=1:3; [fit_error,fitSolutuions] = B{m}.computeLikelihood; B{m}.makeFitPlot(fitSolutuions); end
+for m=1:3
+    [fit_error,fitSolutions] = B{m}.computeLikelihood; 
+    B{m}.makeFitPlot(fitSolutions); 
+end
 
 %% Find MLE for each simulated data set.
 MLE = zeros(3,2,A.ssaOptions.Nexp); fMLE = inf(3,2,A.ssaOptions.Nexp);
@@ -76,8 +80,11 @@ for iExp = 1:A.ssaOptions.Nexp
     B{3} = B{3}.loadData('ToggleSSAData50Expts.csv',{'lacI',['exp',num2str(iExp),'_s1_Distorted'];'lambdaCI',['exp',num2str(iExp),'_s2_Distorted']}); % Link non-distorted data.
     for m=1:3
         B{m}.fittingOptions.modelVarsToFit = [2,3];
-        if iExp==1; x0 = [B{m}.parameters{B{m}.fittingOptions.modelVarsToFit,2}]';
-        else; x0 = squeeze(MLE(m,:,iExp-1)); end
+        if iExp==1
+            x0 = [B{m}.parameters{B{m}.fittingOptions.modelVarsToFit,2}]';
+        else
+            x0 = squeeze(MLE(m,:,iExp-1)); 
+        end
         [MLE(m,:,iExp),fMLE(m,:,iExp)] = B{m}.maximizeLikelihood(x0);
     end
 end
