8fe1efc48153038b37d0ab4b60ddc76dbe492da8
Munsky <munsky@engr.colostate.edu>
2024-09-13 12:39:09 -0600

    Add Example files for Cook Et Al, "Sequential Design..."

---
+++ Examples/Cook_etal_2024_Sequential_Design/Run_All_Analyses.m
@@ -0,0 +1,377 @@
+% This script re-generated all figures associated with the manuscript:
+%    J Cook, E Ron, D Svetlov, LU Aguilera, B Munsky, "Sequential design of
+%    single-cell experiments to identify discrete stochastic models for
+%    gene expression."
+%
+%%
+close all
+clear all
+addpath(genpath('../../src'));
+addpath('codes')
+
+%% Specific which figures to generate and then run script.
+Figure_to_Generate = 3;
+switch Figure_to_Generate
+    case 1  % Figure 1.
+        %%   Generate (or load saved) results
+        [TestCasesBFBD,finalExperimentDesignBFBD] = sequentialExptDesignBatchRunner('Poisson',1,1,true,false,[4]);
+        [TestCasesRD,finalExperimentDesignRD] = sequentialExptDesignBatchRunner('Poisson',2,1,true,false,[4]);
+        close(1,2,3,4,103)
+        %%   Reformat Figure 1B Results (Convergence of |COV|).
+        f = figure(101);
+        set(f,'Position',[ 616   659   429   259])
+        set(gca,'ylim',[1e-9,1e-4],'xlim',[1.5,8],'XTick',[2:8])
+        grid on
+
+        legs = {'BFBD: MHA','BFBD: FIM$_{\rm Est}^{-1}$','BFBD: FIM$_{\rm True}^{-1}$',...
+            'RD: MHA','RD: FIM$_{\rm Est}^{-1}$','RD: FIM$_{\rm True}^{-1}$'};
+        legend(legs,'Interpreter','latex')
+        xlabel('Experiment Round','Interpreter','latex')
+        ylabel('Determinant of Covariance','Interpreter','latex')
+        %%   Print |COV| values at specific rounds.
+        clc
+        disp(['|COV| after 8 rounds for RD = ',num2str(TestCasesRD.detCov(8))])
+        disp(['|COV| after 5 rounds for BFBD = ',num2str(TestCasesBFBD.detCov(5))])
+
+        %%   Figure 1D, Top (Optimized Experiment Design)
+        f2 = figure;
+        set(f2,'Position',[939   687   404   238])
+        colormap('sky')
+        sz = size(finalExperimentDesignBFBD);
+        pCol = finalExperimentDesignBFBD; pCol(end+1,end+1) = 0;
+        pcolor([-0.25:0.5:10.25],[0.5:10.5],pCol)
+        cb = colorbar;
+        cb.Label.FontSize = 16;
+        cb.Label.String = 'Number of Cells';
+        cb.Label.Interpreter = 'latex';
+        set(gca,'FontSize',16,'TickLabelInterpreter','latex');
+        xlabel('Measurement Time (min)','Interpreter','latex')
+        ylabel('Input ($\mu$M)','Interpreter','latex')
+
+        %%   Figure 1D, Bottom (Random Experiment Design)
+        f3 = figure;
+        set(f3,'Position',[939   687   404   238])
+        colormap('sky')
+        sz = size(finalExperimentDesignRD);
+        pCol = finalExperimentDesignRD; pCol(end+1,end+1) = 0;
+        pcolor([-0.25:0.5:10.25],[0.5:10.5],pCol)
+        cb = colorbar;
+        cb.Label.String = 'Number of Cells';
+        cb.Label.Interpreter = 'latex';
+        cb.Label.FontSize = 16;
+        set(gca,'FontSize',16,'TickLabelInterpreter','latex');
+        xlabel('Measurement Time (min)','Interpreter','latex')
+        ylabel('Input ($\mu$M)','Interpreter','latex')
+
+
+        %%   Figure 1C (Scatter plots MHA, Round 4)
+        fs = [122,130];
+        for i = fs
+            f122 = figure(i);
+
+            set(f122,'Position',[1098         576         560         420])
+            subplot(2,2,1);
+            lx1 = [0.9 1.25];
+            lx2 = [-0.6 -0.35];
+            lx3 = [0.6 0.95];
+            set(gca,'FontSize',16,'TickLabelInterpreter','latex','xlim',lx2,'ylim',lx1);
+            ylabel('$\log_{10} (k_r)$','Interpreter','latex')
+            xlabel('$\log_{10} (\gamma)$','Interpreter','latex')
+
+            subplot(2,2,2);
+            set(gca,'FontSize',16,'TickLabelInterpreter','latex','ylim',lx1,'xlim',lx3);
+            xlabel('$\log_{10} (k_D)$','Interpreter','latex')
+
+            subplot(2,2,4);
+            set(gca,'FontSize',16,'TickLabelInterpreter','latex','ylim',lx2,'xlim',lx3);
+            ylabel('$\log_{10} (\gamma)$','Interpreter','latex')
+            xlabel('$\log_{10} (k_D)$','Interpreter','latex')
+        end
+
+        %%
+    case 2  % Figure 2
+        clear all
+        close all
+
+        %%   Generate or load data
+        [TestCasesBFBD,finalExperimentDesignBFBD] = sequentialExptDesignBatchRunner('Burst',1,2,true,false,[4]);
+        [TestCasesRD,finalExperimentDesignRD] = sequentialExptDesignBatchRunner('Burst',2,2,true,false,[4]);
+        close(1,2,3,4,503)
+        %%   Figure 2B (Convergence of |COV|)
+        f = figure(501);
+        set(f,'Position',[ 616   659   429   259])
+        set(gca,'xlim',[2,8],'XTick',[2:8],'YLim',[1e-13,1e-8])
+        grid on
+
+        legs = {'BFBD: MHA','BFBD: FIM$_{\rm Est}^{-1}$','BFBD: FIM$_{\rm True}^{-1}$',...
+            'RD: MHA','RD: FIM$_{\rm Est}^{-1}$','RD: FIM$_{\rm True}^{-1}$'};
+        legend(legs,'Interpreter','latex')
+        xlabel('Experiment round','Interpreter','latex')
+        ylabel('Determinant of Covariance','Interpreter','latex')
+
+        %%   Figure 2D, Top (Optimized Experiment Design)
+        f2 = figure;
+        set(f2,'Position',[939   687   404   238])
+        colormap('sky')
+        sz = size(finalExperimentDesignBFBD);
+        pCol = finalExperimentDesignBFBD; pCol(end+1,end+1) = 0;
+        pcolor([-0.5:1:30.5],[0.5:10.5],pCol)
+        cb = colorbar;
+        cb.Label.FontSize = 16;
+        cb.Label.String = 'Number of Cells';
+        cb.Label.Interpreter = 'latex';
+        set(gca,'FontSize',16,'TickLabelInterpreter','latex');
+        xlabel('Measurement Time (min)','Interpreter','latex')
+        ylabel('Input ($\mu$M)','Interpreter','latex')
+
+        %%   Figure 1D, Bottom (Random Experiment Design)
+        f3 = figure;
+        set(f3,'Position',[939   687   404   238])
+        colormap('sky')
+        sz = size(finalExperimentDesignRD);
+        pCol = finalExperimentDesignRD; pCol(end+1,end+1) = 0;
+        pcolor([-0.5:1:30.5],[0.5:10.5],pCol)
+        cb = colorbar;
+        cb.Label.FontSize = 16;
+        cb.Label.String = 'Number of Cells';
+        cb.Label.Interpreter = 'latex';
+        set(gca,'FontSize',16,'TickLabelInterpreter','latex');
+        xlabel('Measurement Time (min)','Interpreter','latex')
+        ylabel('Input ($\mu$M)','Interpreter','latex')
+
+        %%   Figure 2C (Scatter plots MHA, Round 4)
+        fs = [522,530];
+        for i = fs
+            f122 = figure(i);
+
+            set(f122,'Position',[389         143        1155         852])
+            subplot(5,5,9);
+            lx1 = [-1.2 -0.7];
+            lx2 = [-0.9 -0.3];
+            lx3 = [0.8 1.2];
+            lx4 = [-0.7 -0.35];
+            lx5 = [0 1];
+            set(gca,'FontSize',16,'TickLabelInterpreter','latex','xlim',lx5,'ylim',lx2);
+            ylabel('$\log_{10} (k_{\rm off}^{0})$','Interpreter','latex')
+            xlabel('$\log_{10} (k_{\rm D})$','Interpreter','latex')
+
+            subplot(5,5,13);
+            set(gca,'FontSize',16,'TickLabelInterpreter','latex','ylim',lx3,'xlim',lx4);
+            ylabel('$\log_{10} (k_{\rm r})$','Interpreter','latex')
+            xlabel('$\log_{10} (\gamma_{\rm r})$','Interpreter','latex')
+
+        end
+
+        %%
+    case 3  % Figure 3
+        clear all
+        close all
+        %%   Generate or load data
+        [TestCasesBFBD,finalExperimentDesignBFBD] = sequentialExptDesignBatchRunner('GR',1,2,true,false,[4]);
+        [TestCasesRD,finalExperimentDesignRD] = sequentialExptDesignBatchRunner('GR',2,2,true,false,[4]);
+        
+        %%   Figure 3B (Convergence of |COV|)
+        f = figure(801);
+        set(f,'Position',[ 616   659   429   259])
+        set(gca,'xlim',[1,6],'XTick',[2:6],'YLim',10.^[-8.5,-6])
+        grid on
+        legs = {'BFBD: MHA','BFBD: FIM$_{\rm Pre}^{-1}$','BFBD: FIM$_{\rm Post}^{-1}$',...
+            'RD: MHA','RD: FIM$_{\rm Pre}^{-1}$','RD: FIM$_{\rm Post}^{-1}$'};
+
+        Ch = get(gca,'Children');
+        set(Ch(5),'Marker','o','MarkerSize',20);
+        set(Ch(2),'Marker','o','MarkerSize',20);
+
+        legend(legs,'Interpreter','latex','FontSize',13)
+        xlabel('Experiment round','Interpreter','latex')
+        ylabel('Determinant of Covariance','Interpreter','latex')
+
+        %%   Figure 2E,(Optimized Experiment Design)
+        f2 = figure;
+        set(f2,'Position',[939   687   404   238])
+        colormap('sky')
+        sz = size(finalExperimentDesignBFBD);
+        pCol = finalExperimentDesignBFBD; pCol(end+1,end+1) = 0;
+        pcolor([-0:1:6],[0:3],pCol)
+        cb = colorbar;
+        cb.Label.FontSize = 16;
+        cb.Label.String = 'Number of Cells';
+        cb.Label.Interpreter = 'latex';
+        set(gca,'FontSize',16,'TickLabelInterpreter','latex',...
+            'ytick',[0.5:1:3.5],'YTickLabel',[1,10,100],...
+            'xtick',[0.5:1:5.5],'XTickLabel',[0,10,30,50,75,180]);
+        xlabel('Measurement Time (min)','Interpreter','latex')
+        ylabel('Input ($\mu$M)','Interpreter','latex')
+
+        %%   Figure 3C (Scatter plots MHA, Round 4)
+        fs = [822];
+        for i = fs
+            f122 = figure(i);
+
+            set(f122,'Position',[389         143        1155         852])
+            lx{1} = [-2.4 -2.1];
+            lx{2} = [1.1, 1.25];
+            lx{3} = [-2 -1.7];
+            lx{4} = [-1.95 -1.75];
+            lx{5} = [-2.35 -2.2];
+            lx{6} = [0.9 1.1];
+
+            labs = {'k_{\rm cn}^0';'k_{\rm cn}^{1}';'k_{\rm nc}';'k';'\gamma_{\rm nuc}';'M_{\rm Dex}'};
+
+            for ii = 1:5
+                for jj = ii:5
+                    k = (ii-1)*5+jj;
+                    subplot(5,5,k);
+                    set(gca,'FontSize',16,'TickLabelInterpreter','latex','xlim',lx{jj+1},'ylim',lx{ii});
+                    ylabel('$\log_{10} (k_{\rm off}^{0})$','Interpreter','latex')
+                    ylabel(['$',labs{ii},'$'],'Interpreter','latex')
+                    xlabel(['$',labs{jj+1},'$'],'Interpreter','latex')
+                end
+            end
+        end
+
+
+        %%   Figure 3F (Make Predictions)
+        %%       Solve Predictions Model
+        ModelTrue = SSIT;
+        ModelTrue.species = {'cytGR';'nucGR'};
+        ModelTrue.initialCondition = [20;1];
+        ModelTrue.fspOptions.bounds = [0,0,30,30];
+        ModelTrue.fspOptions.verbose = false;
+        ModelTrue.fspOptions.fspIntegratorAbsTol = 1e-10;
+        ModelTrue.propensityFunctions = {'kcn0*(1 + (t>0)*kcn1*IDex/(MDex+IDex)) * cytGR';...
+            'knc*nucGR'; 'kg1';'gnuc*nucGR'};
+        ModelTrue.stoichiometry = [-1,1,1,0;...
+            1,-1,0,-1];
+        ModelTrue.customConstraintFuns = {'x1+x2'};
+
+        ModelTrue.parameters = {'kcn0',1;'kcn1',1;'knc',1;...
+            'kg1',1;'gnuc',1;'MDex',1};
+
+        % Update to fitted parameters.
+        round4Predictions = 6;
+        ModelTrue.parameters(:,2) = num2cell(TestCasesBFBD.pars(round4Predictions,:));
+        ModelTrue.inputExpressions = {'IDex','10'};
+
+        ModelTrue.fspOptions.initApproxSS = true;
+        ModelTrue.fspOptions.usePiecewiseFSP = true;
+        ModelTrue.fspOptions.constantJacobian = true;
+        ModelTrue.fspOptions.constantJacobianTime = 0.1;
+        ModelTrue = ModelTrue.formPropensitiesGeneral(['Predictor'],true);
+
+        for i=1:3
+            ModelTrue.fspOptions.fspTol = 1e-8;
+            [fspSoln,ModelTrue.fspOptions.bounds] = ModelTrue.solve;
+        end
+
+        ModelTrue = ModelTrue.loadData("ExampleData/Data.csv",...
+            {'nucGR','normgrnuc';'cytGR','normgrcyt'},...
+            {'Dex_Conc','10'});
+
+        %%       Make  figures.
+        close(1,2,3,4,803,830)
+        ModelTrue.makeFitPlot([],1);
+        %%       Reformat Fig 3F,Bottom (Nuclear Distributions)
+        origFigs = [2];
+        titles = {'Nuc GR 10nM'};
+        newFigs = [1022];
+
+        for idex = 1
+            oldFig = figure(origFigs(idex));
+            newFig = figure(newFigs(idex));clf
+            set(newFig,'Position',[1000        1039        1361         199])
+
+            set(newFig,'Name',titles{idex})
+            % Get handles of all subplots in the original figure
+            subplotHandles = findobj(oldFig, 'type', 'axes');
+
+            % Iterate over each subplot and copy its contents to the new figure
+            orderset = [7,6,5,4,3,2,1];
+            for i2 = 1:length(orderset)
+                i = orderset(i2);
+                subplotHandle = subplotHandles(i);
+
+                % Create new subplot in the new figure
+                ax = subplot(1, 7, i2, 'Parent', newFig);
+
+                % Copy contents of the original subplot to the new subplot
+                copyobj(allchild(subplotHandle), ax);
+
+                if i2~=1
+                    ylabel('')
+                    set(gca,'yticklabels',[])
+                end
+
+                h = gca;
+                set(h,'Children',h.Children([1,2,3,4]))
+
+                grid on
+
+                h.Children(3).Visible = 'off';
+                h.Children(4).Visible = 'off';
+
+                h.Children(1).LineWidth = 4;
+                h.Children(2).LineWidth = 4;
+
+                % Adjust concentration scale for Nuclear GR.
+                ratio = 0.520900129552663; %Fit in "ProcessEric_Data_Feb6".
+                for ich=1:2
+                    h.Children(ich).XData = h.Children(ich).XData/ratio;
+                end
+
+                set(gca,'xlim',[0,40],'ylim',[0,0.3],'FontSize',16)
+
+            end
+
+        end
+
+        %%       Reformat Fig 3F,Top (Cytoplasmic Distributions
+        origFigs = [2];
+        titles = {'Cyt GR 10nM'};
+        newFigs = [1032];
+
+        for idex = 1:1
+            oldFig = figure(origFigs(idex));
+            newFig = figure(newFigs(idex));clf
+            set(newFig,'Position',[1000        1039        1361         199])
+
+            set(newFig,'Name',titles{idex})
+            % Get handles of all subplots in the original figure
+            subplotHandles = findobj(oldFig, 'type', 'axes');
+
+            % Iterate over each subplot and copy its contents to the new figure
+            orderset = [7,6,5,4,3,2,1];
+            for i2 = 1:length(orderset)
+                i = orderset(i2);
+                subplotHandle = subplotHandles(i);
+
+                % Create new subplot in the new figure
+                ax = subplot(1, 7, i2, 'Parent', newFig);
+
+                % Copy contents of the original subplot to the new subplot
+                copyobj(allchild(subplotHandle), ax);
+
+                if i2~=1
+                    ylabel('')
+                    set(gca,'yticklabels',[])
+                end
+
+                h = gca;
+                set(h,'Children',h.Children([1,2,3,4]))
+
+                grid on
+
+                h.Children(3).LineWidth = 4;
+                h.Children(4).LineWidth = 4;
+
+                h.Children(1).Visible = 'off';
+                h.Children(2).Visible = 'off';
+
+                set(gca,'xlim',[0,20],'ylim',[0,0.5],'FontSize',16)
+
+            end
+        end
+        close(1,2,3,4)
+
+end
+
---
+++ Examples/Cook_etal_2024_Sequential_Design/codes/sequentialExptDesignBatchRunner.m
@@ -0,0 +1,260 @@
+function [TestCases,finalExperimentDesign] = sequentialExptDesignBatchRunner(iModel,iDesign,rngSeed, makePlots, testing, plotTimes)
+arguments
+    iModel
+    iDesign
+    rngSeed
+    makePlots = false
+    testing  = false
+    plotTimes = []
+end
+
+nRounds = 8;
+initialExperiment = [];
+nFIMsamples = 10;
+
+jobID = 1000*iModel+100*iDesign+rngSeed;
+
+switch iDesign
+    case 1
+        design = 'fimopt';
+    case 2
+        design = 'random';
+end
+incrementAdd = 10;
+switch iModel
+    case 'Poisson' % Poisson
+        %% Poisson Model With Chainging Input
+        truePars = {'kr',10;'gr',0.3;'kD',5};
+        inputLibrary = {{'IDex','1'},{'IDex','2'},{'IDex','3'},{'IDex','4'},{'IDex','5'},{'IDex','6'},...
+            {'IDex','7'},{'IDex','8'},{'IDex','9'},{'IDex','10'}};
+        model = 'poisson';
+        numCellsPerExperiment = 60;
+        initialParGuess = ones(1,3);
+        datType = 'simulated';
+        nInputs = length(inputLibrary);
+        nT = 21;
+        initialExperiment = zeros(nInputs,nT);
+        initialExperiment(1,[1,7,14,21]) = round(numCellsPerExperiment/4);
+
+        parset = [1:size(truePars,1)];
+        iModel = 1;
+
+    case 'Burst' % Uncertain Burst
+        %% Burst Model With Unknown Control Mechanism
+        truePars = ({'kon',0.1;'koff',0.2;'kr',10;'gr',0.3;'M',4;'alph',1e-4});
+        % inputLibrary = {{'IDex','0.1+1*(t>=0)'},{'IDex','0.1+2*(t>=0)'},...
+        %     {'IDex','0.1+3*(t>=0)'},{'IDex','0.1+4*(t>=0)'},...
+        %     {'IDex','0.1+5*(t>=0)'},{'IDex','0.1+6*(t>=0)'},...
+        %     {'IDex','0.1+7*(t>=0)'},{'IDex','0.1+8*(t>=0)'},...
+        %     {'IDex','0.1+9*(t>=0)'},{'IDex','0.1+10*(t>=0)'}};
+        inputLibrary = {{'IDex','1'},{'IDex','2'},{'IDex','3'},{'IDex','4'},{'IDex','5'},{'IDex','6'},...
+            {'IDex','7'},{'IDex','8'},{'IDex','9'},{'IDex','10'}};
+        model = 'uncertainBurst2';
+        numCellsPerExperiment = 300;
+        initialParGuess = 10.^[-1 -1 1 -1 1 0];
+        % initialParGuess = [truePars{:,2}];
+        datType = 'simulated';
+        nInputs = length(inputLibrary);
+        nT = 31;
+        initialExperiment = zeros(nInputs,nT);
+        initialExperiment([2,6,10],[1,6,11,16]) = 50;
+
+        parset = [1:5];
+        iModel = 5;
+    case 'GR' % GR Reduced
+         %% GR Model (Real Data)
+        nRounds = 8;
+        truePars = [];
+
+        inputLibrary = {{'IDex','1'},{'IDex','10'},{'IDex','100'}};
+        initialParGuess = 10.^[-2 1 -2 -2 -2 1];
+        model = 'GR';
+        numCellsPerExperiment = 300;
+        datType = 'real';
+
+        incrementAdd = 50;
+
+        initialExperiment = zeros(3,6);
+        initialExperiment(3,[1,3,6]) = 150;
+        initialExperiment(1,[1,3,6]) = 150;
+
+        parset = [2,5,6];
+        iModel = 8;
+end
+
+
+TestCases = struct('model',{model},...
+    'expDesign',{design},...
+    'numCellsPerExperiment',{numCellsPerExperiment},...
+    'figureSet',{iModel},...
+    'incrementAdd',{incrementAdd},...
+    'truePars',{truePars},...
+    'saveFileNamePrefix',{['results/mod_',model,'_',num2str(iDesign),design,'_',num2str(rngSeed),'seed']}, ...
+    'initialParameterGuess',{initialParGuess},...
+    'inputLibrary',{inputLibrary},...
+    'dataType',{datType});
+
+%% Run Sequential Design Simulations
+TestCases.saveName = iterativeExperimentRunnerMultiConditions(TestCases.model,...
+        TestCases.dataType,...
+        TestCases.expDesign,nRounds,rngSeed,jobID,...
+        TestCases.incrementAdd,...
+        TestCases.numCellsPerExperiment,...
+        initialExperiment,nFIMsamples,...
+        TestCases.truePars,...
+        TestCases.saveFileNamePrefix,...
+        TestCases.initialParameterGuess,...
+        TestCases.inputLibrary,...
+        testing);
+
+if ~makePlots
+    return
+end
+
+
+%% Load results and make plots
+load(TestCases.saveName)
+nRounds = 0;
+while nRounds<length(covLogMH)&&~isempty(covLogMH{nRounds+1})
+    nRounds=nRounds+1;
+end
+
+for iRound = 1:nRounds
+    TestCases.detCov(iRound) = det(covLogMH{iRound}(parset,parset));
+    TestCases.pars(iRound,:) = parametersFound{iRound};
+
+    TestCases.FIMpredNextExpt = FIMpredNextExpt;
+    TestCases.MHResultsSaved = MHResultsSaved;
+
+    [TestCases.detFIMInv_Pred(iRound),...
+        TestCases.stdDetFIMInv_Pred(iRound),...
+        TestCases.minDetFIMInv_Pred(iRound),...
+        TestCases.maxDetFIMInv_Pred(iRound)] = ...
+        predictCov(FIMpredNextExpt{iRound},parset);
+    
+    [TestCases.detFIMInv_Post(iRound),...
+        TestCases.stdDetFIMInv_Post(iRound),...
+        TestCases.minDetFIMInv_Post(iRound),...
+        TestCases.maxDetFIMInv_Post(iRound)] = ...
+        predictCov(FIMcurrentExptSaved{iRound},parset);
+
+    TestCases.detFIMTrueInv(iRound) = predictCov(FIMcurrentExptTrueSaved{iRound},parset);
+    
+    TestCases.FIMTrue(iRound) = FIMcurrentExptTrueSaved{iRound};
+
+    TestCases.measurements = exptDesigns;
+end
+
+cols = ['rkbg'];
+%% Make figures of det(COV) vs experiment round
+for j = 1:3
+    fset = TestCases.figureSet;
+    f = figure(fset*100+1);
+    set(f,'Name',[TestCases.model,' performance'])
+    switch j
+        case 1
+            plot(1:nRounds,TestCases.detCov,'linewidth',2,'Color',cols(iDesign)); hold on;
+        case 2
+            plot(2:nRounds,TestCases.detFIMInv_Pred(1:end-1),'--','linewidth',2,'Color',cols(iDesign)); hold on;
+            % errorbar(2:nRounds,TestCases.detFIMInv_Pred(1:end-1),...
+            %     TestCases.stdDetFIMInv_Pred(1:end-1),...
+            %     '--','linewidth',2,'Color',cols(iDesign)); hold on;
+            % errorbar(2:nRounds,TestCases.detFIMInv_Pred(1:end-1),...
+            %     TestCases.detFIMInv_Pred(1:end-1) - TestCases.minDetFIMInv_Pred(1:end-1),...
+            %     TestCases.maxDetFIMInv_Pred(1:end-1) - TestCases.detFIMInv_Pred(1:end-1),...
+            %     '--','linewidth',2,'Color',cols(iDesign)); hold on;
+        case 3
+            if strcmp(datType,'simulated')
+                plot(1:nRounds,TestCases.detFIMTrueInv,'s','linewidth',2,'MarkerSize',16,'Color',cols(iDesign),'MarkerFaceColor',cols(iDesign)); hold on;
+            else
+                plot(1:nRounds,TestCases.detFIMInv_Post,'s','linewidth',2,'MarkerSize',16,'Color',cols(iDesign),'MarkerFaceColor',cols(iDesign)); hold on;
+            end          
+    end
+    set(gca,"FontSize",16,'yscale','log')
+    legend(TestCases([TestCases.figureSet]==fset).expDesign)
+
+end
+xlabel('Experiment round');
+
+%% Make Figures for MH Results and FIM Analyses
+fset = TestCases.figureSet;
+for iExpt = plotTimes
+    f = figure(fset*100+10+iDesign*length(FIMpredNextExpt)+iExpt); 
+    set(f,'Name',[TestCases.model,', ',TestCases.expDesign,', round ',num2str(iExpt)])
+    if iExpt==1
+        SSIT.plotMHResultsStatic([],...
+            TestCases.MHResultsSaved{iExpt}, ...
+            TestCases.FIMTrue{iExpt},...
+            'log','log10',f,true);
+    else
+        SSIT.plotMHResultsStatic([],...
+            TestCases.MHResultsSaved{iExpt}, ...
+            TestCases.FIMpredNextExpt{iExpt-1},...
+            'log','log10',f,true);
+    end
+    % hold on;
+    % npars = size(TestCases.truePars,1);
+    % for iy = 1:npars-1
+    %     for ix = iy+1:npars
+    %         subplot(npars-1,npars-1,ix-1+(iy-1)*(npars-1))
+    %         ylims = get(gca,'YLim')+[-1,1];
+    %         xlims = get(gca,'XLim')+[-1,1];
+    %         plot(log10(TestCases.truePars{ix,2})*[1,1],ylims,'k--','LineWidth',2)
+    %         plot(xlims,log10(TestCases.truePars{iy,2})*[1,1],'k--','LineWidth',2)
+    %     end
+    % end
+end
+
+%% Make figure of parameter convergence
+linspecs = ['--';'-.';'--';'-.'];
+cols = ['rbrb'];
+fset = TestCases.figureSet;
+f = figure(fset*100+3);
+
+set(f,'Name',['Parameters, ' TestCases.model])
+semilogy(TestCases.pars,linspecs(iDesign,:),'LineWidth',3,'Color',cols(iDesign));hold on;
+for jpar = 1:size(TestCases.truePars,1)
+    plot([1,size(TestCases.pars,1)],TestCases.truePars{jpar,2}*[1,1],'k--')
+end
+
+
+%% Make figures of final experiment designs
+% fset = TestCases.figureSet;
+% f = figure(fset*100+2);
+% set(f,'Name',[TestCases.model,' experiment design, ',TestCases.expDesign])
+finalExperimentDesign = initialExperiment;
+if iModel==8
+    for j = 1:min(nRounds-1,5)
+        finalExperimentDesign = finalExperimentDesign + TestCases.measurements{j};
+    end
+
+else
+
+    for j = 1:nRounds-1
+        finalExperimentDesign = finalExperimentDesign + TestCases.measurements{j};
+    end
+end
+% bar(finalExperimentDesign'); hold on;
+% set(gca,"FontSize",16)
+% xlabel('Measurement Times')
+% ylabel('Number of Cells Measured')
+end
+
+function [predictedDetCov,stdvDetCov,minDetCov,maxDetCov] = predictCov(fimSet,parSet)
+arguments
+    fimSet
+    parSet = [1:length(fimSet{1})];
+end
+detCovPreds = zeros(1,length(fimSet));
+for i = 1:length(fimSet)
+    covEst = fimSet{i}^(-1);
+    detCovPreds(i) = det(covEst(parSet,parSet));
+end
+predictedDetCov = mean(detCovPreds);
+stdvDetCov = std(detCovPreds);
+minDetCov = min(detCovPreds);
+maxDetCov = max(detCovPreds);
+end
+
+% plot(2:nRounds,detFIMTrueInv_FIM,'k',2:nRounds,detFIMTrueInv_FIM,'--k',2:nRounds,detFIMTrueInv_FIM,'-.k','linewidth',2)
+% legend('|COV|','|FIM^-1|','|FIM^-1| True')
