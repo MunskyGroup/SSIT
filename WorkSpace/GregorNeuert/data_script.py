f8e80273ea44adac387ac06eaa97d38a74d081a6
Alex Popinga <alexpopinga@gmail.com>
2024-10-08 05:34:32 +1300

    Add rename function to data_script.py to change 'Time_index_min' to 'time' for SSIT.m readability

--- WorkSpace/GregorNeuert/data_script.py
+++ WorkSpace/GregorNeuert/data_script.py
@@ -5,14 +5,17 @@ file_path = 'pdoCalibrationData_NeuertLab_240925.csv'
 data = pd.read_csv(file_path)
 
 # Filter the data based on Condition = '2M_NaCl_Step'
-# Adjusting the filter to be case-insensitive and checking for partial matches
 filtered_data = data.loc[data['Condition'].str.contains('2M_NaCl_Step', case=False, na=False), 
-                         ['Cell_id', 'Condition', 'Replica', 'Conc_mM', 'time', 'RNA_STL1_cyto_TS3Full']]
-                         
+                         ['Cell_id', 'Condition', 'Replica', 'Conc_mM', 'Time_index_min', 'RNA_STL1_cyto_TS3Full']]
+
+filtered_data.rename(columns={'Time_index_min': 'time'}, inplace=True)
+
+
 # Define the output file path
-output_file_path = 'filtered_data_2M_NaCl_Step.csv'
+output_file = 'filtered_data_2M_NaCl_Step.csv'
+
 
 # Save the filtered data to a new CSV file
-filtered_data.to_csv(output_file_path, index=False)
+filtered_data.to_csv(output_file, index=False)
 
-output_file_path
+output_file
