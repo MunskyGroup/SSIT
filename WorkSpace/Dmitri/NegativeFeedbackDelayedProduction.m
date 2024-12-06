3d81f8d680c6800ae82a5367a14256eeb8bdd398
Dmitri Svetlov <123213937+dmitrisvetlov@users.noreply.github.com>
2024-12-05 20:19:59 -0700

    Add draft of NFDP model

---
+++ WorkSpace/Dmitri/NegativeFeedbackDelayedProduction.m
@@ -0,0 +1,37 @@
+close all
+clear all
+addpath(genpath('../../src'));
+
+Model = SSIT;
+Model.species = {'Dempty'; 'Dfull'; 'X'};
+Model.initialCondition = [1; 0; 0];
+Model.parameters = ({...
+    'kPlus', 100; ... % (Delayed) rate of protein production
+    'kMinus', 1; ... % Rate of protein degradation
+    'kMinus1', 2; ... % Rate of operator site emptying
+    'k1', 1000; ... % Rate of operator site filling
+    'tau', 50 ... % Delay in protein production
+    });
+
+Model.propensityFunctions = {
+    'kMinus1*Dfull', ... % Operator site emptying
+    'k1*Dempty*X', ... % Operator site filling
+    'kPlus*Dempty', ... % (Delayed) protein production
+    'kMinus*X' ... % Protein degradation
+    };
+
+Model.stoichiometry = [...
+     1, -1,  0,  0; ... % Dempty
+    -1,  1,  0,  0; ... % Dfull
+     0,  0,  1, -1 ... % X
+    ];
+
+% The only delayed reaction in the system is the third, with a delay equal
+% to tau:
+
+Model.delayedReactions = [3, Model.parameters{5,2}];
+
+Model.tSpan = linspace(0, 400, 401);
+
+Model.solutionScheme = 'SSA';
+SSASoln = Model.solve;
