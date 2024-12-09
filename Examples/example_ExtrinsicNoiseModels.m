7bd2546f75bc4b15f62d4663f7f42abca2a8e806
Alex Popinga <alexpopinga@gmail.com>
2024-12-08 21:35:29 -0700

    Model1.formPropensitiesGeneral must precede Model1.solve
    Lest the user receive error:
    
    >> example_ExtrinsicNoiseModels
    Unable to perform assignment because the left and right sides have a different number of elements.
    
    Error in ssit.FspMatrixTerm.buildAFSP (line 291)
                aij(1:n_states) = -1.0*prop_val;
    
    Error in ssit.FspMatrixTerm.GenerateMatrixTermTimeInvariant (line 211)
                A_fsp = ssit.FspMatrixTerm.buildAFSP(n_states,prop_val,reachableIndices,propensity,...
    
    Error in ssit.FspMatrixTerm (line 87)
                    obj.matrix = ssit.FspMatrixTerm.GenerateMatrixTermTimeInvariant(propensity, parameters, stateSet, numConstraints, varNames, false); % The matrix data is generated only once
    
    Error in ssit.FspMatrix/regenerate (line 109)
                    obj.terms{i} = ssit.FspMatrixTerm(propensities{i}, parameters, stateSet, numConstraints, varNames, modRedTransformMatrices, computeSens);
    
    Error in ssit.FspMatrix (line 65)
                obj = obj.regenerate(propensities, parameters, stateSet, numConstraints, varNames, modRedTransformMatrices, computeSens);
    
    Error in ssit.fsp.adaptiveFspSolve (line 197)
        AfspFull = ssit.FspMatrix(propensities, parameters, stateSpace, constraintCount, speciesNames);
    
    Error in SSIT/solve (line 872)
                        [Solution.fsp, bConstraints,Solution.stateSpace] = ssit.fsp.adaptiveFspSolve(obj.tSpan,...
    
    Error in example_ExtrinsicNoiseModels (line 18)
    [fspSoln1,Model1.fspOptions.bounds] = Model1.solve;

--- Examples/example_ExtrinsicNoiseModels.m
+++ Examples/example_ExtrinsicNoiseModels.m
@@ -2,21 +2,21 @@
 % In this example, we show how to sample an FSM  model over intrinsic noise
 % in its various parameters. 
 close all 
-clear all
+clear
 addpath(genpath('../src'));
+
 %% Example 1 - transcription and translation
 % First create a full model (e.g., for mRNA and protein)
-Model1 = SSIT();
+Model1 = SSIT;
 Model1.species = {'rna','protein'};
 Model1.initialCondition = [0;0];
 Model1.propensityFunctions = {'kr';'gr*rna';'k2*rna';'g2*protein'};
-Model1.stoichiometry = [1,-1,0,0;0,0,1,-1];
-Model1.parameters = ({'kr',10;'gr',0.5;...
-    'k2',2;'g2',1});
-Model1.fspOptions.initApproxSS = false; 
+Model1.stoichiometry = [1,-1,0,0; 0,0,1,-1];
+Model1.parameters = ({'kr',10;'gr',0.5;'k2',2;'g2',1});
+Model1.solutionScheme = 'FSP';
 Model1.tSpan = linspace(0,5,10);
-[fspSoln1,Model1.fspOptions.bounds] = Model1.solve;
-Model1 = Model1.formPropensitiesGeneral('Model1');   
+Model1 = Model1.formPropensitiesGeneral('Model1');
+[fspSoln1,Model1.fspOptions.bounds] = Model1.solve; 
 Model1.makePlot(fspSoln1,'marginals',[],[],[2,3])
 
 %% Generate, solve and plot results for an extrinsic noise version
