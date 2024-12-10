427709a99bb27e4c7b40b8aa4ab378fc037ed79b
Dmitri Svetlov <123213937+dmitrisvetlov@users.noreply.github.com>
2024-12-09 22:01:04 -0700

    Add some statistics and plotting code

--- WorkSpace/Dmitri/NegativeFeedbackDelayedProduction.m
+++ WorkSpace/Dmitri/NegativeFeedbackDelayedProduction.m
@@ -4,20 +4,33 @@ close all
 clear all
 addpath(genpath('../../src'));
 
+M = [1 2 3 6 10]; % Number of intermediate states
+
+kOn = 0.1;
+kOff = kOn * 1000 / 2;
+
+tau = 50;
+delayPeriods = 10;
+tSpan = linspace(0, tau * delayPeriods, 1 + delayPeriods);
+
 Model = SSIT;
 Model.species = {'Dempty'; 'Dfull'; 'X'};
-Model.initialCondition = [1; 0; 0];
-Model.parameters = ({...
+
+initialCondition = [1; 0; randi(50)];
+Model.initialCondition = initialCondition;
+
+parameters = ({...
     'kPlus', 100; ... % (Delayed) rate of protein production
     'kMinus', 1; ... % Rate of protein degradation
-    'kMinus1', 2; ... % Rate of operator site emptying
-    'k1', 1000; ... % Rate of operator site filling
-    'tau', 2 ... % Delay in protein production
+    'kOn', kOn; ... % Rate of operator site emptying / activation
+    'kOff', kOff; ... % Rate of operator site filling / deactivation
+    'tau', tau ... % Delay in protein production
     });
+Model.parameters = parameters;
 
 Model.propensityFunctions = {
-    'kMinus1*Dfull', ... % Operator site emptying
-    'k1*Dempty*X', ... % Operator site filling
+    'kOn*Dfull', ... % Operator site emptying
+    'kOff*Dempty*X', ... % Operator site filling
     'kPlus*Dempty', ... % (Delayed) protein production
     'kMinus*X' ... % Protein degradation
     };
@@ -42,12 +55,56 @@ Model.delayedReactionSchedulingS = [...
 
 %% Configure and run SSA
 
-%Model.tSpan = linspace(0, 400, 401);
-Model.tSpan = linspace(0, 9, 10);
+Model.tSpan = tSpan;
 Model = Model.formPropensitiesGeneral('NFDP');
 
 Model.solutionScheme = 'SSA';
-SSASoln = Model.solve;
+Model.ssaOptions.Nexp = 2;   % Number of independent data sets to generate.
+Model.ssaOptions.nSimsPerExpt = 1000; % Number of cells to include at each time point for each data set.
+tic;
+SSAsoln = Model.solve;
+SSAtime = toc;
+
+save("SSAsoln", SSAsoln);
 
 %% Plot trajectories
-Model.makePlot(SSASoln,'trajectories',[],[],4) % Make some plots.
+Model.makePlot(SSAsoln,'trajectories',[],[]) % Make some plots.
+
+%% Protein intermediates
+
+piFSPtimes = zerosLike(M);
+for Mcntr = 1:length(M)
+    curM = M(Mcntr);
+    piModel = getNFModelWithProteinIntermediates(...
+        curM, initialCondition, parameters);
+    piModel.tSpan = tSpan;
+    piModel.solutionScheme = 'FSP';
+    tic;
+    [piFSPsoln, piModel.fspOptions.bounds] = piModel.solve;
+    piFSPtimes(Mcntr) = toc;
+
+    piModel.makePlot(piFSPsoln,'meansAndDevs',[],[]) % Make plot of mean vs. time.
+    piModel.makePlot(piFSPsoln,'marginals',[],[]) % Make plot of marginals
+end
+
+%% Operator intermediates
+
+opFSPtimes = zerosLike(M);
+for Mcntr = 1:length(M)
+    curM = M(Mcntr);
+    opModel = getNFModelWithOperatorIntermediates(...
+        curM, initialCondition, parameters);
+    opModel.tSpan = tSpan;
+    opModel.solutionScheme = 'FSP';
+    tic;
+    [piFSPsoln, opModel.fspOptions.bounds] = opModel.solve;
+    opFSPtimes(Mcntr) = toc;
+
+    opModel.makePlot(piFSPsoln,'meansAndDevs',[],[]) % Make plot of mean vs. time.
+    opModel.makePlot(piFSPsoln,'marginals',[],[]) % Make plot of marginals
+end
+
+disp(piFSPtimes)
+save("piFSPtimes", piFSPtimes);
+disp(opFSPtimes)
+save("opFSPtimes", opFSPtimes);
