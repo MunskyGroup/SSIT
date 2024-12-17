c6431c19de3b78228b66912caeb3082cf1732fe8
Dmitri Svetlov <123213937+dmitrisvetlov@users.noreply.github.com>
2024-12-17 09:18:10 -0700

    Add version of OI not starting at defaults

---
+++ WorkSpace/Dmitri/oi_fsp_nondefault.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --time=3:00:00
+#SBATCH --partition=csu
+#SBATCH --qos=normal
+#SBATCH --ntasks=32
+#SBATCH --job-name=OI_FSP_Nondefault
+#SBATCH --output=OI_FSP_Nondefault.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=svetlov@colostate.edu
+
+module purge
+
+module load matlab/R2023b
+
+matlab -nodesktop -nodisplay -r 'clear; OI_FSP_NonDefault;'
