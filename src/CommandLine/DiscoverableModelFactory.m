727878edf6b4326c8150c47011c9cf061d38a165
Dmitri Svetlov <Dmitri.Svetlov@colostate.edu>
2025-06-16 15:34:51 -0600

    Comment out propensity-forming to enable other testing

--- src/CommandLine/DiscoverableModelFactory.m
+++ src/CommandLine/DiscoverableModelFactory.m
@@ -116,7 +116,7 @@ classdef DiscoverableModelFactory
                         diag(m.SigmaLog10Prior.^2*log(10^2));
 
                     m.inputExpressions = {'IDex','100'};
-                    m = m.formPropensitiesGeneral(append(dataFilename,"_S",num2str(ind)),true);
+                    %m = m.formPropensitiesGeneral(append("GRTest","_S",num2str(ind)),true);
                     % m = {m};
                 % case {"GR", "GRFewer", "GRFewest"} ...
             end % switch arch
