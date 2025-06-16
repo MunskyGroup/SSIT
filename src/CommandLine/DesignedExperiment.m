3ac7d3ed592bae9805af33cc6f062a061b4c3397
Dmitri Svetlov <123213937+dmitrisvetlov@users.noreply.github.com>
2025-06-16 13:15:17 -0600

    Add model-related properties to experiment

--- src/CommandLine/DesignedExperiment.m
+++ src/CommandLine/DesignedExperiment.m
@@ -1,5 +1,25 @@
 classdef DesignedExperiment
     properties
-        Configuration (1, 1) ExperimentConfiguration
+        % The BaseModel should be assigned by the object owning the
+        % experiment. It reflects the model as it should be used EXCEPT FOR
+        % any modification(s) that the configuration might make.        
+        BaseModel (1, 1) DiscoverableModel
+        Configuration (1, 1) ExperimentConfiguration       
+    end
+
+    properties (Dependent)
+        % The Model should be queried by any object referencing the
+        % experiment, e.g. data-fitting and simulation routines. It 
+        % includes any modification(s) that the configuration might make.
+        % Being a dependent property, it is recalculated upon demand to
+        % reflect the latest values of both the base model and the
+        % configuration.
+        Model (1, 1) DiscoverableModel
+    end
+
+    methods
+        function model = get.Model(obj)
+            model = obj.Configuration.applyToModel(obj.BaseModel);
+        end
     end
 end
