91bbd3980a2f16a690495f52ac0b266f27c31561
Dmitri Svetlov <123213937+dmitrisvetlov@users.noreply.github.com>
2025-06-16 13:16:59 -0600

    Add # of observations and ability to export round to CSV

--- src/CommandLine/SequentialExperimentRound.m
+++ src/CommandLine/SequentialExperimentRound.m
@@ -1,14 +1,25 @@
-classdef SequentialExperimentRound
-    properties
+classdef SequentialExperimentRound  
+    properties (SetAccess = private)
         Experiments (1, :) DesignedExperiment
-    end
-    properties %(Access = private)
         NumberOfConfigs (1, 1) double {mustBeInteger, mustBePositive} = 1
         NumberOfConfigurables (1, 1) double {mustBeInteger, mustBePositive} = 1
         VarNames (1, :) string {mustBeNonempty} = 'VarNamePlaceholder'
         VarTypes (1, :) string {mustBeNonempty} = 'VarTypePlaceholder'
     end
+
+    properties (Dependent)
+        NumberOfObservations
+    end
+
     methods
+        function observations = get.NumberOfObservations(obj)
+            observations = 0;
+            for experimentIdx = 1:length(obj.Experiments)
+                observations = observations + ...
+                    obj.Experiments(experimentIdx).Configuration.NumberOfObservations;
+            end
+        end
+
         function obj = SequentialExperimentRound(configs)
             arguments
                 configs (1, :) ExperimentConfiguration
@@ -38,6 +49,21 @@ classdef SequentialExperimentRound
                     configs(configIdx);
             end
         end
+
+        function filename = exportAsCSV(obj, filename)            
+            if isempty(filename)
+                filename = tempname();
+            end
+            [path, name, ext] = fileparts(filename);
+            if isempty(ext)
+                ext = '.csv';
+            end
+            filename = fullfile(path, strcat(name, ext));
+
+            t = obj.exportAsTable();
+            writetable(t, filename);
+        end % exportAsCSV
+
         function t = exportAsTable(obj)
             t = table(...
                 'Size', [obj.NumberOfConfigs (obj.NumberOfConfigurables + 1)], ...
@@ -50,6 +76,6 @@ classdef SequentialExperimentRound
                 end
                 t(configIdx, end) = {obj.Experiments(configIdx).Configuration.NumberOfObservations};
             end
-        end
-    end
+        end % exportAsTable
+    end % methods
 end
