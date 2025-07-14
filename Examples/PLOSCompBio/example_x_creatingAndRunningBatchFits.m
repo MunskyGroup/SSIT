76eee4c99982deddc150e1f77214e11e4660d7da
Munsky <munsky@engr.colostate.edu>
2025-07-14 15:20:20 -0600

    Add example for launching jobs using an SBATCH command.

--- Examples/PLOSCompBio/example_x_creatingAndRunningBatchFits.m
+++ Examples/PLOSCompBio/example_x_creatingAndRunningBatchFits.m
@@ -8,6 +8,7 @@ DataSettings = {'data/STL1.csv',{'mRNA','rna'}};
 
 % Create model from preset, associate with data.
 Model = SSIT('BirthDeath',[],DataSettings);
+Model = Model.formPropensitiesGeneral('BirthDeath');
 
 % Save model for later use.
 saveFile = 'generatedModel.mat';
@@ -29,3 +30,15 @@ cmd = SSIT.generateCommandLinePipeline(saveFile,'Model',[],Pipeline,pipelineArgs
 
 %% Run that command from matlab.
 system(cmd);
+
+%% Run on cluster
+% Note that this is set up for a specific cluster that uses sbatch. To run
+% on your own cluster, you will need to change the argument
+% 'clusterPrefix' below.
+% Generate the command so you can run it from terminal.
+% cmd = SSIT.generateCommandLinePipeline(saveFile,'Model',[],Pipeline,pipelineArgs,saveFile,'logFile.txt',0,1)
+
+% Run it directly.
+% cmd = SSIT.generateCommandLinePipeline(saveFile,'Model',[],Pipeline,pipelineArgs,saveFile,'logFile.txt',1,1)
+
+
