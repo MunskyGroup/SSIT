285161f0fb97fce0025b293712bdecf9baf38834
Dmitri Svetlov <123213937+dmitrisvetlov@users.noreply.github.com>
2025-07-22 21:16:26 -0600

    Add "true model" properties for use in simulated experiments

--- src/CommandLine/DesignedExperiment.m
+++ src/CommandLine/DesignedExperiment.m
@@ -6,6 +6,12 @@ classdef DesignedExperiment
         % experiment. It reflects the model as it should be used EXCEPT FOR
         % any modification(s) that the configuration might make.        
         UnderlyingModel (1, 1) DiscoverableModel        
+        % The UnderlyingTrueModel should be assigned by the object owning
+        % the experiment. It reflects the true model as it should be used
+        % EXCEPT FOR any modification(s) that the configuration might make.
+        % Being the ground-truth model, it can only meaningfully be
+        % accessed in a purely simulated context. 
+        UnderlyingTrueModel (1, 1) DiscoverableModel        
     end
 
     properties (Dependent)
@@ -16,11 +22,23 @@ classdef DesignedExperiment
         % reflect the latest values of both the underlying model and the
         % configuration.
         Model (1, 1) DiscoverableModel
+        % The TrueModel should be queried by any object referencing the
+        % experiment, e.g. data-fitting and simulation routines. It 
+        % includes any modification(s) that the configuration might make.
+        % Being a dependent property, it is recalculated upon demand to
+        % reflect the latest values of both the underlying true model and
+        % the configuration.
+        TrueModel (1, 1) DiscoverableModel
     end
 
     methods
         function model = get.Model(obj)
             model = obj.Configuration.applyToModel(obj.UnderlyingModel);
+        end
+        function model = get.TrueModel(obj)
+            model = obj.Configuration.applyToModel(obj.UnderlyingTrueModel);
+            model.ssaOptions.nSimsPerExpt = obj.Configuration.NumberOfObservations;
+            model.ssaOptions.Nexp = 1;
         end
     end
 end
