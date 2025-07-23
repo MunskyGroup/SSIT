bdf3397306b0dd13f7e32d0f01213280c0da2eca
Dmitri Svetlov <123213937+dmitrisvetlov@users.noreply.github.com>
2025-07-22 21:13:15 -0600

    Condense comments on design approach

--- src/CommandLine/AbstractSequentialExperimentDesignStrategy.m
+++ src/CommandLine/AbstractSequentialExperimentDesignStrategy.m
@@ -4,25 +4,26 @@ classdef AbstractSequentialExperimentDesignStrategy
         ObservationQuantum (1, 1) double {mustBeInteger, mustBePositive} = 10
     end
     methods
-        function round = designRound(obj, round)
-            % Step 1. Import all relevant data collected thus far, i.e.
-            % all data pertaining to the configuration of each experiment
-            % in the round.
-
-            % Step 2. Perform any required analysis specific to the 
-            % design strategy.
+        function round = designRound(obj, round)            
+            % Step 1. Perform any required analysis specific to the 
+            % design strategy, including (if appropriate) the importing and
+            % fitting of all relevant data collected thus far, i.e. all
+            % data pertaining to the configuration of each experiment in 
+            % the round.
             
             round = obj.designRoundInternal(round);
 
-            % Step 3. Apportion the available/desired number of
+            % Step 2. Apportion the available/desired number of
             % observations across the experiments in the round.
 
             round = obj.apportionObservations(round);
         end
     end
+
     methods (Abstract)
         round = apportionObservations(obj, round)        
     end
+    
     methods (Access = protected)
         function round = designRoundInternal(~, round)
             arguments
