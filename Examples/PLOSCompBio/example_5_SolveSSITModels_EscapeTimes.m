43b83ff86af403e894e7d0e6ed6b97d3c51e8470
Alex Popinga <alexpopinga@gmail.com>
2025-10-06 19:49:54 -0600

    Separate plots

--- Examples/PLOSCompBio/example_5_SolveSSITModels_EscapeTimes.m
+++ Examples/PLOSCompBio/example_5_SolveSSITModels_EscapeTimes.m
@@ -7,14 +7,14 @@
 
 %% Preliminaries
 % Use the models from example_1_CreateSSITModels
-%clear
-%close all
-addpath(genpath('../../'));
+% clear
+% close all
+% addpath(genpath('../../'));
 
 % example_1_CreateSSITModels
 
 % Load the models created in example_1_CreateSSITModels
-load('example_1_CreateSSITModels.mat')
+% load('example_1_CreateSSITModels.mat')
 
 % View model summaries:
 Model.summarizeModel
@@ -42,7 +42,10 @@ STL1_4state.tSpan = linspace(0,50,200);
     Model_escape.fspOptions.escapeSinks.b = 5;
     Model_escape = Model_escape.formPropensitiesGeneral('Model_escape');
     [fspSoln_escape_1,Model_escape.fspOptions.bounds] = Model_escape.solve;
-    Model_escape.makePlot(fspSoln_escape_1,'escapeTimes')
+
+    % Plot the CDF and PDF
+    fig1 = figure(1); clf; set(fig1,'Name','Bursting Gene');
+    Model_escape.makePlot(fspSoln_escape_1,'escapeTimes',[],false,fig1)
     legend(Model_escape.fspOptions.escapeSinks.f)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -51,32 +54,40 @@ STL1_4state.tSpan = linspace(0,50,200);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% STL1:
-% Create a copy of the time-varying STL1 yeast model:
-STL1_escape = STL1;
-STL1_escape = STL1_escape.formPropensitiesGeneral('STL1_escape');
+    % Create a copy of the time-varying STL1 yeast model:
+    STL1_escape = STL1;
+    STL1_escape = STL1_escape.formPropensitiesGeneral('STL1_escape');
+    
+    % Solve for the escape time:
+    STL1_escape.fspOptions.escapeSinks.f = {'offGene';'onGene'};
+    STL1_escape.fspOptions.escapeSinks.b = [0.5;0.5];
+    [STL1_fspSoln_escape,STL1_escape.fspOptions.bounds] = ...
+        STL1_escape.solve;
 
-% Solve for the escape time:
-STL1_escape.fspOptions.escapeSinks.f = {'offGene';'onGene'};
-STL1_escape.fspOptions.escapeSinks.b = [0.5;0.5];
-[STL1_fspSoln_escape,STL1_escape.fspOptions.bounds] = STL1_escape.solve;
-STL1_escape.makePlot(STL1_fspSoln_escape,'escapeTimes')
-legend(STL1_escape.fspOptions.escapeSinks.f)
+    % Plot the CDF and PDF
+    fig2 = figure(2); clf; set(fig2,'Name','STL1');
+    STL1_escape.makePlot(STL1_fspSoln_escape,'escapeTimes',[],false,fig2)
+    legend(STL1_escape.fspOptions.escapeSinks.f)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Ex(3): Solve escape times for the 4-state time-varying STL1 yeast model
 %  from example_1_CreateSSITModels
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%% STL1 4-state:
-% Create a copy of the time-varying STL1 yeast model:
-STL1_4state_escape = STL1_4state;
-STL1_4state_escape = ...
-    STL1_4state_escape.formPropensitiesGeneral('STL1_4state_escape');
+%% 4-state STL1:
+    % Create a copy of the time-varying STL1 yeast model:
+    STL1_4state_escape = STL1_4state;
+    STL1_4state_escape = ...
+        STL1_4state_escape.formPropensitiesGeneral('STL1_4state_escape');
+    
+    % Solve for the escape time:
+    STL1_4state_escape.fspOptions.escapeSinks.f = {'g4'}
+    STL1_4state_escape.fspOptions.escapeSinks.b = [0.5];
+    [STL1_4state_fspSoln_escape,STL1_4state_escape.fspOptions.bounds] = ...
+        STL1_4state_escape.solve;
 
-% Solve for the escape time:
-STL1_4state_escape.fspOptions.escapeSinks.f = {'g4'}
-STL1_4state_escape.fspOptions.escapeSinks.b = [0.5];
-[STL1_4state_fspSoln_escape,STL1_4state_escape.fspOptions.bounds] = ...
-    STL1_4state_escape.solve;
-STL1_4state_escape.makePlot(STL1_4state_fspSoln_escape,'escapeTimes')
-legend(STL1_4state_escape.fspOptions.escapeSinks.f)
+    % Plot the CDF and PDF
+    fig3 = figure(3); clf; set(fig3,'Name','4-state STL1');
+    STL1_4state_escape.makePlot(STL1_4state_fspSoln_escape,...
+                                'escapeTimes',[],false,fig3)
+    legend(STL1_4state_escape.fspOptions.escapeSinks.f)
