6ed785fa067d43053251088839a01ed28d7f2f0c
Munsky <munsky@engr.colostate.edu>
2025-12-04 14:41:52 -0700

    Change how propensity functions codes are written.

---
+++ src/CommandLine/matlabFunctionSSIT.m
@@ -0,0 +1,56 @@
+function handleOut = matlabFunctionSSIT(symbolicExpression,VarNames,fn,isSparse,logVarsReps)
+% This function writes m files for SSIT propensity functions.
+arguments
+    symbolicExpression
+    VarNames
+    fn
+    isSparse = false;
+    logVarsReps = {};
+end
+
+jFilName = max([strfind(fn,'/'),strfind(fn,'\')]);
+
+fid = fopen(fn, 'w');
+
+txt = ['function result = ',fn(jFilName+1:end-2),'(in1,in2,in3,in4)\r\n'];
+fprintf(fid, txt);
+
+fprintf(fid,['%%This is an automatically generated on ',datestr(now),'.\r\n']);
+fprintf(fid,'%%\r\n');
+
+fprintf(fid,'arguments\r\n');
+fprintf(fid,'  in1\r\n');
+fprintf(fid,'  in2\r\n');
+fprintf(fid,'  in3 = [];\r\n');
+fprintf(fid,'  in4 = [];\r\n');
+
+fprintf(fid,'end\r\n');
+fprintf(fid,'\r\n');
+
+for j = 1:length(VarNames)
+    for i = 1:length(VarNames{j})
+        fprintf(fid,[char(VarNames{j}(i)),'=in',num2str(j),'(',num2str(i),',:);\r\n']);
+    end
+    fprintf(fid,'\r\n');
+end
+
+txt = ['result = ',char(symbolicExpression),';\r\n'];
+
+if ~isempty(logVarsReps)
+    % Replace first string in function name
+    for i = 1:size(logVarsReps,1)
+        txt = strrep(txt,[logVarsReps{i,2}],['(',logVarsReps{i,1},')']);
+    end
+
+    txt = strrep(txt,', ''omitnan'', false','');
+    txt = strrep(txt,', \"''omitnan''\", false','');
+
+end
+
+fprintf(fid,txt);
+fclose(fid);
+
+handleOut = str2func(fn(jFilName+1:end-2));
+
+
+
