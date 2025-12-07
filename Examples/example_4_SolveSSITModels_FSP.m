2cedb5f529f0821e78de906fe35a0683c0cc2e06
Munsky <munsky@engr.colostate.edu>
2025-12-07 12:13:36 -0700

    Fix bounds guess in example 4.

--- Examples/example_4_SolveSSITModels_FSP.m
+++ Examples/example_4_SolveSSITModels_FSP.m
@@ -44,7 +44,7 @@ STL1_4state.tSpan = linspace(0,50,200);
     Model_FSP.fspOptions.fspTol = 1e-4; 
     
     % Guess initial bounds on FSP StateSpace:
-    Model_FSP.fspOptions.bounds = [1,1,400];
+    Model_FSP.fspOptions.bounds = [0,0,0,1,1,400];
     
     % Have FSP approximate the steady state for the initial distribution 
     % by finding the eigenvector corresponding to the smallest magnitude 
@@ -81,7 +81,7 @@ STL1_4state.tSpan = linspace(0,50,200);
     STL1_FSP.fspOptions.fspTol = 1e-4; 
     
     % Guess initial bounds on FSP StateSpace:
-    STL1_FSP.fspOptions.bounds = [1,1,400];
+    STL1_FSP.fspOptions.bounds = [0,0,0,1,1,400];
     
     % Have FSP approximate the steady state for the initial distribution 
     % by finding the eigenvector corresponding to the smallest magnitude 
@@ -118,7 +118,7 @@ STL1_4state.tSpan = linspace(0,50,200);
     STL1_4state_FSP.fspOptions.fspTol = 1e-4; 
     
     % Guess initial bounds on FSP StateSpace:
-    STL1_4state_FSP.fspOptions.bounds = [1,1,1,1,400];
+    STL1_4state_FSP.fspOptions.bounds = [0,0,0,0,0,1,1,1,1,400];
     
     % Have FSP approximate the steady state for the initial distribution 
     % by finding the eigenvector corresponding to the smallest magnitude 
