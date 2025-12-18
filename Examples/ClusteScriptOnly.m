2e588f4f7c245552810c9cbc8f7380fabcb0bd93
Munsky <munsky@engr.colostate.edu>
2025-12-18 15:47:34 -0700

    Add shortened example of codes to run on cluster.

---
+++ Examples/ClusteScriptOnly.m
@@ -0,0 +1,19 @@
+addpath(genpath('../src'));
+%% Call Pipeline to Fit Model
+
+% Specify pipeline to apply to model and arguments
+% ("../../SSIT/src/exampleData/examplePipelines/fittingPipelineExample.m") 
+Pipeline = 'fittingPipelineExample';
+pipelineArgs.maxIter = 1000;
+pipelineArgs.display = 'iter';
+pipelineArgs.makePlot = false;
+pipelineArgs.nRounds = 5;
+
+%% Launch cluster jobs for all genes.
+for iGene = 1:length(geneNames)
+    modelName = ['Model_',geneNames{iGene}];
+    saveName = ['seqModels/',modelName];    
+    logfile = ['logFiles/log',modelName];
+    cmd = SSIT.generateCommandLinePipeline(saveName,modelName,[],Pipeline,...
+        pipelineArgs,saveName,logfile,1,1);
+end
