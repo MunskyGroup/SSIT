d8c4061ecc80cd5c9a39552436243d9d00c0593a
Alex Popinga <alexpopinga@gmail.com>
2026-03-25 15:53:59 -0600

    Update example0_Complete_Manuscript.m

--- Examples/example0_Complete_Manuscript.m
+++ Examples/example0_Complete_Manuscript.m
@@ -336,19 +336,24 @@ STL1_4state.plotFits(plotType="all", lineProps={'linewidth',2},...
 % Make a copy of our 4-state STL1 model:
 STL1_4state_MH = STL1_4state;
 
+% Specify how many model parameters will be fit (the rest will be fixed):
+fitpars = 13;
+
+% Choose parameters to search:
+STL1_4state_MH.fittingOptions.modelVarsToFit = [1:fitpars]; 
+
+% Specify Bayesian Prior and fit:
 % Specify Prior as log-normal distribution with wide uncertainty
+
 % Prior log-mean:
 mu_log10 = [0.5,2,5,3.5,-0.4,1,0.2,0.4,-0.5,-1.3,-0.1,2,0.5]; 
 
 % Prior log-standard deviation:
-sig_log10 = 2*ones(1,13);  
+sig_log10 = 2*ones(1,fitpars);  
 
 % Prior:
 STL1_4state_MH.fittingOptions.logPrior = ...
     @(x)-sum((log10(x)-mu_log10).^2./(2*sig_log10.^2));
-
-% Choose parameters to search:
-STL1_4state_MH.fittingOptions.modelVarsToFit = [1:13]; 
 
 % Create first parameter guess:
 STL1_4state_MH_pars = [STL1_4state_MH.parameters{:,2}];      
@@ -357,11 +362,13 @@ STL1_4state_MH_pars = [STL1_4state_MH.parameters{:,2}];
 %  Running a few rounds of MLE and MH together may improve convergence:
 
 STL1_4state_MH.parameters(:,2) = num2cell(STL1_4state_MH_pars);
+
 for i=1:2
     % Maximize likelihood:
     STL1_4state_MH_pars = STL1_4state_MH.maximizeLikelihood([]);    
+
     % Update parameters in the model:
-    STL1_4state_MH.parameters(:,2) = num2cell(STL1_4state_MH_pars);
+    STL1_4state_MH.parameters([1:fitpars],2) = num2cell(STL1_4state_MH_pars);
 
     % Run Metropolis-Hastings    
     proposalWidthScale = 0.01;
@@ -375,18 +382,16 @@ for i=1:2
 
     % Run Metropolis-Hastings: 
     [STL1_4state_MH_pars,~,STL1_4state_MH_MHResults] = ...
-        STL1_4state_MH.maximizeLikelihood([], MHOptions,...
-        'MetropolisHastings');
+      STL1_4state_MH.maximizeLikelihood([],MHOptions,'MetropolisHastings');
     
     % Store MH parameters in model:
-    STL1_4state_MH.parameters([1:13],2) = ...
-        num2cell(STL1_4state_MH_pars);
+    STL1_4state_MH.parameters([1:fitpars],2) = num2cell(STL1_4state_MH_pars);
 end
 
 % Plot results:
 STL1_4state_MH.plotMHResults(STL1_4state_MHResults);
 
-STL1_4state_MH.plotFits(plotType="all",lineProps={'linewidth',2},...
+STL1_4state_MH.plotFits(plotType="all", lineProps={'linewidth',2},...
     Title='4-state STL1 (MH)', YLabel='Molecule Count',...
     LegendLocation='northeast', LegendFontSize=18, ProbXLim = [0 80],...
     TimePoints=[0 8 10 15 30 55], TitleFontSize=24, AxisLabelSize=20);
