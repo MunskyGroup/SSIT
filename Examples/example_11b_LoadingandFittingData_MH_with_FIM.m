904ce6ff9aacb88afdac46ddd6296cd59a806223
Munsky, Brian <munsky@colostate.edu>
2026-07-21 06:53:27 -0600

    Fix FIM based sampling in Example 11b.

--- Examples/example_11b_LoadingandFittingData_MH_with_FIM.m
+++ Examples/example_11b_LoadingandFittingData_MH_with_FIM.m
@@ -5,7 +5,7 @@
 %   * Uncertainty sampling using the Metropolis-Hastings Algorithm (MHA)
 %   * Use Bayesian priors and iterate between computing MLE and MH
 %   * Use FIM for Metropolis-Hastings proposal distribution. 
-%     This sometimes provides faster mixing (convergence).
+%     This often provides faster mixing (convergence).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Preliminaries
@@ -26,9 +26,6 @@
 load('example_7_FIM.mat')
 load('example_10_LoadingandFittingData_MLE.mat')
 
-% Make a new copy of our 4-state STL1 model:
-STL1_4state_MH_FIM = STL1_4state;
-
 %% Compute FIM, Run Metropolis Hastings
 % Specify Prior as log-normal distribution with wide uncertainty
 % Prior log-mean:
@@ -38,42 +35,44 @@ mu_log10 = [0.5,2,5,3.5,-0.4,1,0.2,0.4,-0.5,-1.3,-0.1,2,0.5];
 sig_log10 = 2*ones(1,13);      
 
 % Prior:
-STL1_4state_MH_FIM.fittingOptions.logPrior = ...
+STL1_4state.fittingOptions.logPrior = ...
     @(x)-sum((log10(x)-mu_log10).^2./(2*sig_log10.^2));
 
-% Compute total FIM including effect of prior:
-fimTotal = STL1_4state_MH_FIM.evaluateExperiment(fimTotal_free,...
-           STL1_4state_MH_FIM.dataSet.nCells,diag(sig_log10.^2)); 
+% Calculate the FIMs for all time points for the free parameters
+fims_free = STL1_4state.computeFIM(scale='log',observed='mRNA',...
+    freePars=freePars); % FIM sub matrix
+
+% Compute total FIM including effect of prior. Note, this FIM calculation
+% is  made at the current parameter values which were fit to maximize the
+% MLE in example "example_10_LoadingandFittingData_MLE".
+fimTotal = STL1_4state.evaluateExperiment(fims_free,...
+           STL1_4state.dataSet.nCells,diag(sig_log10.^2)*log(10)^2); 
 
 % Select FIM for free parameters:
-FIMfree = fimTotal{1}([1:13],[1:13]); 
+FIMfree = fimTotal{1}; 
 
 % Estimate the covariance using CRLB:
 COVfree = (1/2*(FIMfree + FIMfree'))^(-1);  
 
 % Define Metropolis-Hasting settings:
-STL1_4state_MH_FIM.fittingOptions.logPrior = ...
-    @(x)-sum((log10(x)-mu_log10([1:13])).^2./(2*sig_log10([1:13]).^2));
-proposalWidthScale = 0.01;
+STL1_4state.fittingOptions.logPrior = ...
+    @(x)-sum((log10(x)-mu_log10((1:13))).^2./(2*sig_log10((1:13)).^2));
+proposalWidthScale = 1;
 STL1_4state_MH_FIM_FIMOptions = ...
  struct('proposalDistribution',@(x)mvnrnd(x,proposalWidthScale*COVfree),...
         'numberOfSamples',2000,'burnin',500,'thin',2);
 fitOptions = optimset('Display','iter','MaxIter',2000);
 
-% Compute the MLEs:
-STL1_4state_MH_FIM = ...
-    STL1_4state_MH_FIM.maximizeLikelihood(fitOptions=fitOptions);
-
 % Run Metropolis-Hastings (seeking acceptance ratio around 0.3-0.4): 
-STL1_4state_MH_FIM = STL1_4state_MH_FIM.maximizeLikelihood(...
+STL1_4state = STL1_4state.maximizeLikelihood(...
     fitOptions=STL1_4state_MH_FIM_FIMOptions,...
     fitAlgorithm='MetropolisHastings');
 
-% Plot MH samples, FIM:
-STL1_4state_MH_FIM.plotMHResults(STL1_4state_MH_FIM.Solutions.mhResults,...
+% Plot MH samples and FIM:
+STL1_4state.plotMHResults(STL1_4state.Solutions.mhResults,...
                                   FIM=FIMfree, fimScale='log')
 
-STL1_4state_MH_FIM.plotFits(plotType="all",lineProps={'linewidth',2},...
+STL1_4state.plotFits(plotType="all",lineProps={'linewidth',2},...
     Title='4-state STL1', YLabel='Molecule Count',...
     LegendLocation='northeast', LegendFontSize=12);
 
