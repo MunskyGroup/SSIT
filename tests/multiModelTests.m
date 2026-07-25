d5e63db1702194e6a2a917475e4ac7c7fa19c13b
Dmitri Svetlov <Dmitri.Svetlov@colostate.edu>
2026-07-24 23:52:41 -0600

    Update multi-model tests for changes to data loading

--- tests/multiModelTests.m
+++ tests/multiModelTests.m
@@ -30,7 +30,7 @@ classdef multiModelTests < matlab.unittest.TestCase
             tc.Poiss.ssaOptions.Nexp = 1;
             delete 'testData1.csv'
             tc.Poiss.sampleDataFromFSP(tc.PoissSolution,'testData1.csv');
-            tc.Poiss = tc.Poiss.loadData('testData1.csv',{'rna','exp1_s1'});
+            tc.Poiss = tc.Poiss.loadData('testData1.csv');
 
             tc.Poiss2 = tc.Poiss;
             tc.Poiss2.ssaOptions.nSimsPerExpt = 100;
@@ -40,7 +40,7 @@ classdef multiModelTests < matlab.unittest.TestCase
             [tc.Poiss2Solution,tc.Poiss2.fspOptions.bounds] = tc.Poiss2.solve;
             delete 'testData2.csv'
             tc.Poiss2.sampleDataFromFSP(tc.Poiss2Solution,'testData2.csv');
-            tc.Poiss2 = tc.Poiss2.loadData('testData2.csv',{'rna','exp1_s1'});
+            tc.Poiss2 = tc.Poiss2.loadData('testData2.csv');
 
          end  
     end
