f9ee8b2c24141f04800a335efad39c2e419c96f6
Jack <155103214+Formanj-0@users.noreply.github.com>
2026-09-24 18:25:11 -0600

    Update main_Forman2027.m

--- FormanARB2027/main_Forman2027.m
+++ FormanARB2027/main_Forman2027.m
@@ -13,26 +13,33 @@ Forman.makeFigs1CD(f1C1,f1C2,f1C3,f1D);
 % Forman.exportFigs1(f1B,f1C1,f1C2,f1C3,f1D)
 
 %% Figure 2 A-H
-figure(201); ax2A = gca;
+f2a = figure(201); ax2A = gca;
 Forman.makeFigs2A(ax2A);
 
 Forman.prepareFigs2B;
 %%
-figure(202); ax2B = gca;
-figure(203); ax2C = gca;
-figure(204); ax2D = gca;
+f2b = figure(202); ax2B = gca;
+f2c = figure(203); ax2C = gca;
+f2d = figure(204); ax2D = gca;
 f2E1 = figure(205);
-figure(206); ax2E2 = gca;
+f2e = figure(206); ax2E2 = gca;
 figure(207); ax2F1 = gca;
-figure(208); ax2F2 = gca;
+f2f = figure(208); ax2F2 = gca;
 Forman.makeFigs2BtoF(ax2B,ax2C,ax2D,f2E1,ax2E2,ax2F1,ax2F2,Nsamps=20);
 
 %% 2G - Prepare MLE reuslts
-Forman.prepareFig2G(nMLE=20)
+Forman.prepareFig2G(nMLE=500)
 %% 2G - Make the figures
+f2g1 = figure(209);
+f2g2 = figure(210);
+f2h1 = figure(211);
+f2h2 = figure(212);
 Forman.makeFig2G;
-Forman.makeFig2H;
+Forman.makeFig2H(f2g1, f2g2, f2h1, f2h2);
 
+%% 
+fignums = [f2a, f2b, f2c, f2d, f2e, f2f, f2g1, f2g2, f2h1, f2h2];
+Forman.exportFigs3(fignums,'AnnualReview_Figures',fileType='svg')
 %% Figure 3 A
 f3A1 = figure(301); 
 f3A2 = figure(302);
@@ -65,8 +72,8 @@ f3D2 = figure(315);
 f3D3 = figure(316);
 Forman = Forman.makeFig3D(f3D1,f3D2,f3D3);
 
-% fignums = [f3A1,f3A2,f3A3,f3A4,f3A5,f3B1,f3B2,f3B3,f3B4,f3C1,f3C2,f3C3,f3C4,f3D1,f3D2,f3D3];
-% Forman.exportFigs3(fignums,'AnnualReview_Figures',fileType='pdf')
+fignums = [f3A1,f3A2,f3A3,f3A4,f3A5,f3B1,f3B2,f3B3,f3B4,f3C1,f3C2,f3C3,f3C4,f3D1,f3D2,f3D3];
+Forman.exportFigs3(fignums,'AnnualReview_Figures',fileType='svg')
 
 %% Figure 4ABC 
 f4A = figure(401);
@@ -75,6 +82,9 @@ f4C = figure(403);
 Forman = Forman.makeFig4ABC(f4A,f4B,f4C);
 
 %% Figure 4DEF
-Forman = Forman.prepareFig4DEF(nMLE=20);
+Forman = Forman.prepareFig4DEF(nMLE=500);
 %%
 Forman.makeFigs4DEF;
+
+
+%% Modify figs for export
